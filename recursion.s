7fcbb33f3ff0c0835e71817dcfd807ee71298e40
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-03-22 11:26:54 -0400

    Added the printing enter string section

--- recursion.s
+++ recursion.s
@@ -1,6 +1,14 @@
 .data
 max_input: .space 10000
 not_valid: .asciiz "Invalid Input"
+start1: .asciiz "Enter the string:
+
+.text
+main:
+    main:
+    li $v0, 4
+    la $a0, start1
+    syscall
 
 
 
